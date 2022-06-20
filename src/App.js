e3ee875c3be675a614e872b8d56c37c474e85f68
Cian Butler <ciandevelops@gmail.com>
2022-06-19 21:11:03 -0400

    category icons added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import Category from "./components/Category";
 import Pages from "./pages/Pages";
-
 
 function App() {
   return (
     <div className="App">
-    <Pages/>
+      <Category />
+      <Pages />
     </div>
   );
 }
 
 export default App;
-
